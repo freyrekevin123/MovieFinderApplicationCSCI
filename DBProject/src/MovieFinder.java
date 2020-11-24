0b7a33a5b0ce25fa3a86cae03df700de99149d97
Larry <lgait9784@ung.edu>
2020-11-24 11:29:02 -0500

    Successfully implemented DataScanner into the main method. Removed any previous instances of upload code.

--- DBProject/src/MovieFinder.java
+++ DBProject/src/MovieFinder.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -37,72 +38,13 @@ public class MovieFinder {
 	prepStatement2.executeUpdate(movieCastTableCreationSQL);
 	System.out.println("Created the movie_name table.");
 	
-	//Now to scan in movie-name-score
-	try {
-		Scanner movieNameReader = new Scanner(new FileInputStream("movie-name-score.txt"));
-		
-		
-		//Goes thru the whole file to process the data and upload it
-		int mnCounter = 0;
-		while(movieNameReader.hasNextLine())
-		{
-			String currentLine = movieNameReader.nextLine();
-			
-			
-			//Thank you very much stack overflow: https://stackoverflow.com/questions/18893390/splitting-on-comma-outside-quotes
-			String[] delimitedLine = currentLine.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-			//Following the format of the movie-cast, first two data points will
-			//be integers and the last one will be a name.
-			int movieID = Integer.parseInt(delimitedLine[0]);
-			String movieName = delimitedLine[1];
-			int score = Integer.parseInt(delimitedLine[2]);
-			
-			PreparedStatement postNM = connect.prepareStatement("INSERT INTO movie_name_score (movieID, mname, mscore ) "
-					+ "VALUES(?,?,?)");
-			postNM.setInt(1, movieID);
-			postNM.setString(2, movieName);
-			postNM.setInt(3, score);
-			
-			postNM.executeUpdate();
-			mnCounter++;
-		}
-		movieNameReader.close();
-		System.out.println("inserted "+mnCounter+" lines into movie_name_cast");
-	} catch (FileNotFoundException e) {
-		System.out.println("File was not found. Please make sure its in the correct folder or correctly named.");
-		e.printStackTrace();
-	}
-
-	//Now to scan in movie-cast 
-	try {
-		Scanner castReader = new Scanner(new FileInputStream("movie-cast.txt"));
-		
-		
-		int castCounter = 0;
-		while(castReader.hasNextLine())
-		{
-			String currentLine = castReader.nextLine();
-			String[] delimitedLine = currentLine.split(",");
-			//Following the format of the movie-cast, first two data points will
-			//be integers and the last one will be a name.
-			int movieID = Integer.parseInt(delimitedLine[0]);
-			int castID = Integer.parseInt(delimitedLine[1]);
-			String actorName = delimitedLine[2];
-			PreparedStatement postNM = connect.prepareStatement("INSERT INTO movie_cast (movieID, castID, cname) "
-					+ "VALUES(?,?,?)");
-			postNM.setInt(1, movieID);
-			postNM.setInt(2, castID);
-			postNM.setString(3, actorName);
-			postNM.executeUpdate();
-			castCounter++;
-		}
-		castReader.close();
-		System.out.println("inserted "+castCounter+" lines into movie_cast");
-		
-	} catch (FileNotFoundException e) {
-		System.out.println("File was not found. Please make sure its in the correct folder or correctly named.");
-		e.printStackTrace();
-	}
-
+	File MovieNS = new File("movie-name-score.txt");
+	DataScanner fileScanner = new DataScanner(MovieNS);
+	fileScanner.ScanNameAndUpload();
+	File MovieCast = new File("movie-cast.txt");
+	fileScanner.setFileUpload(MovieCast);
+	fileScanner.scanMovieCastAndUpload();
+	
+	
 	}
 }
